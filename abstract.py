e54c603318e5fd2b17c3fbfa73bbacb8bf4b4893
priyadharshini18996 <59823459+priyadharshini18996@users.noreply.github.com>
2020-03-04 18:01:26 +0530

    oops_abstract

---
+++ abstract.py
@@ -0,0 +1,38 @@
+
+class Animal(object): 
+
+	def move(self): 
+		pass
+
+class Human(Animal): 
+
+	def move(self): 
+		print("I can walk and run") 
+
+class Snake(Animal): 
+
+	def move(self): 
+		print("I can crawl") 
+
+class Dog(Animal): 
+
+	def move(self): 
+		print("I can bark") 
+
+class Lion(Animal): 
+
+	def move(self): 
+		print("I can roar") 
+		
+
+R = Human() 
+R.move() 
+
+K = Snake() 
+K.move() 
+
+R = Dog() 
+R.move() 
+
+K = Lion() 
+K.move() 
